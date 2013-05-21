f96fc5375a6f3f4b831016ad98c2ab3691a3290e
shuson <wlgq@hotmail.com>
2013-05-21 11:15:32 +0900

    add cpu and memory checker

--- checker.py
+++ checker.py
@@ -1,1 +1,15 @@
 import wmi
+
+w=wmi.WMI()
+
+# list all processors' information
+for processor in w.Win32_Processor():
+  print "CPU: %s,%s" %(processor.DeviceID, processor.Name)
+
+#list the total memory
+totalMemSize=0
+for memModule in w.Win32_PhysicalMemory():
+  totalMemSize+=int(memModule.Capacity)
+
+print "Memory: %s" %totalMemSize
+
